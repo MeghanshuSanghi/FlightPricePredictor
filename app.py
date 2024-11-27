3aa7fdb9f21119b4d35d4a1e5c1a8654bf8297ad
MeghanshuSanghi <meghanshusanghi@gmail.com>
2024-11-27 20:40:03 +0530

    new html files for forgot password

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
 import pickle
-from flask import Flask,request,jsonify,render_template, redirect, session, flash
+from flask import Flask,request,render_template, redirect, session, flash, url_for,make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_mail import *
 from random import *
+import string
+from datetime import datetime, timedelta    
+import random
 
 app = Flask(__name__)
 app.config.from_object("config")
@@ -22,12 +25,16 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    reset_token = db.Column(db.String(100), nullable=True)
+    reset_token_expiration = db.Column(db.DateTime, nullable=True) 
 
-    def __init__(self,id,email,password,name):
+    def __init__(self,id,email,password,name, reset_token=None, reset_token_expiration=None):
         self.id = id
         self.name = name
         self.email = email
         self.password = password
+        self.reset_token = reset_token
+        self.reset_token_expiration = reset_token_expiration
 
 
 with app.app_context():
@@ -47,7 +54,7 @@ def register():
         password = request.form['password']
 
         secured_pass = bcrypt.generate_password_hash(password).decode('utf-8')
-        new_user = User(id=id,name=name,email=email,password=secured_pass)
+        new_user = User(id=id,name=name,email=email,password=secured_pass,reset_token=None,reset_token_expiration=None)
         db.session.add(new_user)
         db.session.commit()
 
@@ -57,22 +64,39 @@ def register():
 
 @app.route('/index')
 def index():
+    if 'email' not in session:
+        flash("Please log in to access this page.", "warning")
+        return redirect('/login')
     return render_template('index.html')
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.before_request
+def prevent_cache():
+    if 'email' not in session:
+        response = make_response()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '0'
+        return response
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
 
         user = User.query.filter_by(email=email).first()
-        
+
         if user:
-            if user.password == bcrypt.generate_password_hash(password).decode('utf-8'):
-              session['email'] = user.email
-            return redirect('/index')
+            if bcrypt.check_password_hash(user.password, password):
+                session['email'] = user.email
+                flash('Login successful!', 'success')
+                return redirect('/index')
+            else:
+                flash('Invalid password. Please try again.', 'danger')
+                return render_template('login.html')
         else:
-            flash('Invalid Credentials. Try again')
+            flash('Email not found. Please try again.', 'danger')
             return render_template('login.html')
 
     return render_template('login.html')
@@ -124,7 +148,16 @@ def predict():
     
 @app.route('/index/logout',methods=['GET','POST'])
 def logout():
-    return render_template('login.html')
+    
+    session.clear()
+    flash("You have been logged out successfully.", "success")
+
+    response = make_response(redirect('/login'))
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    
+    return response
 
 @app.route('/verify/<email>',methods = ["GET","POST"])  
 def verify(email):  
@@ -142,5 +175,58 @@ def validate():
         return redirect('/login')   
     return "<h3>Invalid otp entered, Kindly, register again !!</h3>"  
 
+@app.route('/forgot_password',methods=['GET','POST'])
+def fp():
+    if request.method == 'POST':
+        email = request.form['email']
+        user = User.query.filter_by(email=email).first()
+        
+        if user:
+            token = generate_reset_token()
+            expiration_time = datetime.utcnow() + timedelta(hours=1)  # Token expires in 1 hour
+            user.reset_token = token
+            user.reset_token_expiration = expiration_time
+            db.session.commit()
+
+            reset_link = url_for('reset_password', token=token, _external=True)
+            send_reset_email(email, reset_link)
+            return "Check your email for the password reset link."
+        else:
+            return "Email not found."
+
+    return render_template('forgot_password.html')
+
+def generate_reset_token():
+    return ''.join(random.choices( string.ascii_letters + string.digits, k=32))
+
+def send_reset_email(to_email, reset_link):
+    msg = Message('Password Reset Request', sender='mailtesting063@gmail.com', recipients=[to_email])
+    msg.body = f'Click the following link to reset your password: {reset_link}'
+    mail.send(msg)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    user = User.query.filter_by(reset_token=token).first()
+
+    if user:
+        if datetime.utcnow() > user.reset_token_expiration:
+            flash("This token has expired.", "danger")
+            return redirect('/forgot_password')
+
+        if request.method == 'POST':
+            new_password = request.form['password']
+            hashed_password = bcrypt.generate_password_hash(new_password)
+            user.password = hashed_password
+            user.reset_token = None
+            user.reset_token_expiration = None
+            db.session.commit()
+            flash("Your password has been reset successfully.", "success")
+            return redirect('/login')
+
+        return render_template('reset_password.html', token=token)
+
+    else:
+        flash("Invalid or expired reset token.", "danger")
+        return redirect('/forgot_password')
 if __name__=='__main__':
     app.run(debug=True)
